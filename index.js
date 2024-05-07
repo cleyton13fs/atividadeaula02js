786772f640e105e43e9ee2847b3025d615ec6a5f
Clayton Ferreira <146362136+cleyton13fs@users.noreply.github.com>
2024-05-07 10:07:19 -0300

    Add files via upload
    Atividade em sala aula 02

---
+++ index.js
@@ -0,0 +1,15 @@
+//iserir 3 notas 
+//calcule a media do usuario e armazene
+
+let nota1= parseFloat(prompt("Insira a primeira nota:"))
+let nota2= parseFloat(prompt("Insira a segunda nota:"))
+let nota3= parseFloat(prompt("Insira a terceira nota:"))
+let media = (nota1 + nota2 + nota3)/3
+
+
+//faça o mesmo programa e retorne
+let nota1 = parseFloat(prompt("Insira a primeira nota:"))
+let nota2 = parseFloat(prompt("Insira a segunda nota:"))
+let nota3 = parseFloat(prompt("Insira a terceira nota:"))
+let media = (nota1 + nota2 + nota3)/3
+console.log(`Sua média é: ${media}`)
